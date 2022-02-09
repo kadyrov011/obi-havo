76d4a9831a84458b95b8c36819b3652246cca838
kadyrov011 <mkodir84@gmail.com>
2022-02-09 15:56:24 +0500

    ssda

---
+++ js/idex.js
@@ -0,0 +1,61 @@
+let elInfo = document.querySelector('.info__city');
+let  elInput = document.querySelector('.js__input');
+let elListUl = document.querySelector('.elListUl')
+
+
+
+function handleTarget(evt){
+  console.log(evt.target.dataset.lat);
+  fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${evt.target.textContent}&limit=1&appid=d333810d973a09b861b11c830ecb267f`
+  )
+    .then((res) => res.json())
+    .then((data) => data.forEach(element => {
+      var lat = evt.target.dataset.lat = element.lat
+      var lon = evt.target.dataset.lon = element.lon 
+      localStorage.setItem("let",lat);
+      localStorage.setItem("lon",lon);
+      window.location = 'http://127.0.0.1:5500/info.html'
+    }));
+  
+}
+
+elInfo.addEventListener('click',handleTarget)
+ 
+
+function hendleClickBtn(evt){
+  localStorage.setItem('let',evt.target.dataset.lat)
+  localStorage.setItem('lon',evt.target.dataset.lon)
+  window.location = 'http://127.0.0.1:5500/info.html'
+  }
+
+function handleInput(evt){
+  elListUl.innerHTML = ""
+  fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${evt.target.value}&limit=5&appid=d333810d973a09b861b11c830ecb267f`
+  )
+    .then((res) => res.json())
+    .then(data => data.forEach(value => {
+      var elLi = document.createElement('li')
+      var elBtn = document.createElement('button')
+      
+      elBtn.textContent = value.name
+      elBtn.classList.add('btnJs')
+      elLi.classList.add('btnli')
+      var btnLat = elBtn.dataset.lat = value.lat
+      var btnLon = elBtn.dataset.lon = value.lon
+      
+      elBtn.addEventListener("click",hendleClickBtn)
+     
+      elLi.append(elBtn)
+      elListUl.append(elLi)
+
+    
+    })
+    )
+   
+
+}
+
+elInput.addEventListener('input', handleInput)
+
---
+++ js/info.js
@@ -0,0 +1,31 @@
+let elBack = document.querySelector('.js-back')
+let elTitle = document.querySelector(".title")
+let eltext = document.querySelector('.js-p')
+let eltext2 = document.querySelector('.js-p2')
+let eltext3 = document.querySelector('.js-p3')
+let eltext4 = document.querySelector('.js-p4')
+
+
+
+var let = localStorage.getItem("let")
+var lon = localStorage.getItem("lon")
+
+
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${let}&lon=${lon}&appid=d333810d973a09b861b11c830ecb267f`
+      )
+      .then((res) => res.json())
+      .then((data =>{ 
+          console.log(data)
+          elTitle.textContent = data.name
+        eltext.textContent = data.main.temp
+        eltext2.textContent = data.weather[0].main
+        eltext3.textContent = data.wind.speed
+        eltext4.textContent = data.wind.deg
+    }))
+function handleBack() {
+    
+    console.log(window.location = 'http://127.0.0.1:5500/index.html');
+}
+
+elBack.addEventListener('click',handleBack)
